81ea27ab2321c2332380827966240d448eba8b3d
Róberta Andersen <ra@treble.tech>
2023-09-12 20:45:02 +0000

    build WIP

--- React/webpack.config.js
+++ React/webpack.config.js
@@ -1,27 +1,12 @@
-const path = require('path');
-const HWP = require('html-webpack-plugin');
-module.exports = {
-  entry: path.join(__dirname, './src/index.tsx'),
-  output: {
-    filename: 'build.js',
-    path: path.join(__dirname, './dist')},
-    module:{
-        rules:[{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        },
-        {
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            loader: 'ts-loader'
-         }]
-    },
-    plugins:[
-        new HWP({template: path.join(__dirname,'./src/index.html')})
-    ],
-    resolve:
-    {
-        extensions: [ '.tsx', '.ts', '.js' ],
-    }
-}
+const commonConfig = require('./build-utils/webpack.common');
+const argv = require('webpack-nano/argv');
+const { merge } = require('webpack-merge');
+
+
+module.exports = () => {
+
+  const envConfig = require(`./build-utils/webpack.prod.js`);
+  const mergedConfig = merge(commonConfig, envConfig);
+
+  return mergedConfig;
+};
