e5dad20591649bcaa92c1a40e8563ed690dafcf9
Róberta Andersen <roberta.andersen@skra.is>
2023-09-13 14:10:24 +0000

    test

--- React/src/index.tsx
+++ React/src/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './UI/GlobalStyle';
 import Header from './UI/HeaderStyle';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from './Navigation/RouteObjects';
 import Navigation from './Navigation/Navigation';
 
@@ -12,10 +12,12 @@ import Navigation from './Navigation/Navigation';
 createRoot(document.getElementById("root")!)
   .render(
     <React.StrictMode>
-      <GlobalStyle />
-      <Header />
-      <Navigation />
-      <RouterProvider router={createBrowserRouter(routes())} />
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Navigation />
+        {/* <RouterProvider router={createBrowserRouter(routes())} /> */}
+      </BrowserRouter>
     </React.StrictMode>
   );
 
